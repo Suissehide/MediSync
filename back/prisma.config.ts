e8d0e5f6e1d1d585d49924b049fe321c1056d90c
Leo Couffinhal <leo.couffinhal@qwetle.fr>
2026-02-10 00:06:48 +0100

    feat(prisma): add prisma generated in Dockerfile

--- back/prisma.config.ts
+++ back/prisma.config.ts
@@ -1,7 +1,8 @@
+import 'dotenv/config'
 import type { PrismaConfig } from 'prisma'
-import { env } from 'prisma/config'
+import { defineConfig, env } from 'prisma/config'
 
-export default {
+export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
@@ -10,4 +11,4 @@ export default {
   datasource: {
     url: env('DATABASE_URL'),
   },
-} satisfies PrismaConfig
+} satisfies PrismaConfig)
